7ac06ef46e53c694ad20d4f1bbf4bcf7d30d8e19
Soubahil <mohamedasbai@live.be>
2021-08-06 13:16:04 +0200

    database

---
+++ src/main/java/EMFactory.java
@@ -0,0 +1,8 @@
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMFactory {
+    public static EntityManagerFactory getEMF() {
+        return Persistence.createEntityManagerFactory("vroemDatabase");
+    }
+}
---
+++ src/main/java/Secretariat.java
@@ -0,0 +1,2 @@
+public class Secretariat {
+}
